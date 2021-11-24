082366cbf71a046b1461b41cd41aec4dd55bbf69
kevin knight <tgodkev@gmail.com>
2021-11-24 00:25:04 -0500

    start

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer(){
+    return(
+        <div className="Footer">
+            <p>Code by kev 2021</p>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header(){
+return(
+    <div className="Header">
+        <h1>This is my Header</h1>
+    </div>
+
+)
+
+}
+
+export default Header;
